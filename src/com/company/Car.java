ce044a9f9619ac1dfe5468bfa31612f367510883
tfranks247 <tfranks247@gmail.com>
2019-11-04 16:40:14 -0500

    not finished, rewatch tutorial

---
+++ src/com/company/Car.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Car extends Vehicle {
+
+    private int wheels;
+    private int doors;
+    private int gears;
+    private boolean isManuel;
+
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int doors, int gears, boolean isManuel) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManuel = isManuel;
+        this.currentGear = 1;
+    }
+
+    public void changeGear(int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Changed to " + this.currentGear + " gear. ");
+    }
+
+    public void changeVelocity(int speed, int direction){
+
+        System.out.println("Car.changerVelocity() : Velocity " + speed + " direction " + direction);
+        move(speed, direction);
+    }
+
+}
